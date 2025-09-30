50f8112bd20846efdc2637833834ff3951156a41
Konayukiw <konayukiw@outlook.com>
2025-09-30 22:53:01 +0900

    having error for now

--- src/main/java/com/dev/particles/EntityCustomCritFX.java
+++ src/main/java/com/dev/particles/EntityCustomCritFX.java
@@ -10,25 +10,32 @@ public class EntityCustomCritFX extends EntityCrit2FX {
     public EntityCustomCritFX(World worldIn, double x, double y, double z,
                               double xSpeed, double ySpeed, double zSpeed) {
         super(worldIn, x, y, z, xSpeed, ySpeed, zSpeed);
+        this.particleScale *= Particles.particleScale;
     }
 
     @Override
     public void renderParticle(WorldRenderer worldRendererIn, Entity entityIn, float partialTicks,
                                float rotationX, float rotationZ, float rotationYZ,
                                float rotationXY, float rotationXZ) {
-        GlStateManager.pushMatrix();
+        try {
+            GlStateManager.pushMatrix();
 
-        GlStateManager.translate(
-                (float)(this.prevPosX + (this.posX - this.prevPosX) * partialTicks - interpPosX),
-                (float)(this.prevPosY + (this.posY - this.prevPosY) * partialTicks - interpPosY),
-                (float)(this.prevPosZ + (this.posZ - this.prevPosZ) * partialTicks - interpPosZ)
-        );
+            GlStateManager.translate(
+                    (float)(this.prevPosX + (this.posX - this.prevPosX) * partialTicks - interpPosX),
+                    (float)(this.prevPosY + (this.posY - this.prevPosY) * partialTicks - interpPosY),
+                    (float)(this.prevPosZ + (this.posZ - this.prevPosZ) * partialTicks - interpPosZ)
+            );
 
-        GlStateManager.scale(Particles.particleScale, Particles.particleScale, Particles.particleScale);
+            GlStateManager.scale(Particles.particleScale, Particles.particleScale, Particles.particleScale);
 
-        super.renderParticle(worldRendererIn, entityIn, partialTicks,
-                rotationX, rotationZ, rotationYZ, rotationXY, rotationXZ);
+            super.renderParticle(worldRendererIn, entityIn, partialTicks,
+                    rotationX, rotationZ, rotationYZ, rotationXY, rotationXZ);
 
-        GlStateManager.popMatrix();
+        } catch (Exception e) {
+            System.out.println("[Particles] Error while rendering custom crit particle:");
+            e.printStackTrace();
+        } finally {
+            GlStateManager.popMatrix();
+        }
     }
 }
